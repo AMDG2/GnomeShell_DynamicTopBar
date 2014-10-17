51f432ffa4863338efe6102d9b5f3f1b803ad784
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2014-10-17 23:15:07 +0200

    Check that extension tool exists
    Check that gnome-shell-extension-tool actually exists before using it or
    installing any extension.

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! `command -v gnome-shell-extension-tool` ]; then
+    echo "gnome-shell-extension-tool not found!"
+    exit 1
+fi
+
 echo "Copying extension files..."
 cp -r dynamicTopBar@gnomeshell.feildel.fr ~/.local/share/gnome-shell/extensions
 echo "Enabling extension..."
