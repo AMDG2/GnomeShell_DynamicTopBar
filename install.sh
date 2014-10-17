21fe4062897e1103888512d051b670d5524bef61
Baudouin Feildel <b.feildel@gmail.com>
2014-10-17 18:50:09 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Copy the extension files..."
-cp -r dynamicTopBar@gnomeshell.feildel.fr
+cp -r dynamicTopBar@gnomeshell.feildel.fr ~/.local/share/gnome-shell/extensions
 echo "Enable the extension"
 gnome-shell-extension-tool -e dynamicTopBar@gnomeshell.feildel.fr
 
